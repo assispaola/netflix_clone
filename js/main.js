c7dd38df96641d5a9fefd1dcbb1032056f078a3d
Paola Assis <paolasantos.assis@gmail.com>
2022-03-25 02:59:49 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+    // Carrossel
+    document.querySelector("#items").addEventListener("wheel", event => {
+        if(event.deltaY > 0) {
+            event.target.scrollBy(300, 0)
+        } else {
+            console.log("scroll downs")
+        }
+    })
